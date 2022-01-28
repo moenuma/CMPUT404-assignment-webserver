e5bb7df1cc5f21ae132eb70d26b74dee5ac3fa6e
moenuma <moenumasawa@gmail.com>
2022-01-29 02:17:43 +0900

    Delete some comments

--- server.py
+++ server.py
@@ -2,10 +2,6 @@
 import socketserver
 import os
 
-# CMPUT404-W22 Assignment 1
-# Author: Moe Numasawa (numasawa)
-# Submission date: Jan 28, 2022
-#
 # Copyright 2022 Abram Hindle, Eddie Antonio Santos, Moe Numasawa
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
